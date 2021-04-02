cf411b79bd23798d1077951ffd36bb9d100626b4
Ragnheiður Sveinsdóttir <ragnh94@gmail.com>
2021-04-02 13:51:31 +0000

    start setup

---
+++ assignment9/src/main/java/autograder/assignment9/App.java
@@ -0,0 +1,13 @@
+package autograder.assignment9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
