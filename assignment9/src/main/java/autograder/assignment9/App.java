4d0ce362f83c3cfceec4d042b8cea932fe2bcfd2
SteiniSaem <thorsteinns16@ru.is>
2021-04-02 16:56:52 +0000

    map out test cases

--- assignment9/src/main/java/autograder/assignment9/App.java
+++ assignment9/src/main/java/autograder/assignment9/App.java
@@ -17,10 +17,9 @@ import org.apache.poi.ss.usermodel.Workbook;
  */
 public class App 
 {
-	public static String getCellData(FileInputStream fis, Workbook wb, int vRow, int vCol)
+	public static String getCellData(Workbook wb, int vRow, int vCol)
 	{
-		String value = null;          //variable for storing the cell value  
-		Sheet sheet=wb.getSheetAt(0);   //getting the XSSFSheet object at given index  
+		Sheet sheet = wb.getSheetAt(0);   //getting the XSSFSheet object at given index  
 		Row row = sheet.getRow(vRow); //returns the logical row  
 		Cell cell = row.getCell(vCol); //getting the cell representing the given column  
 		try {
@@ -31,20 +30,75 @@ public class App
 		}
 	}
 	
+	public static void mapTestCases(Workbook wb)
+	{
+		Sheet sheet = wb.getSheetAt(0);
+		int TCRowNumbers[] = new int[6];
+		
+		//find rownumbers of lines that contain TC1, TC2, TC3, TC4, TC5
+		for(Row row: sheet) {
+			for(Cell cell : row) {
+				if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
+	                if (cell.getRichStringCellValue().getString().equals("TC1")) {
+	                	TCRowNumbers[0] = row.getRowNum();  
+	                }
+	                else if (cell.getRichStringCellValue().getString().equals("TC2")) {
+	                	TCRowNumbers[1] = row.getRowNum();  
+	                }
+	                else if (cell.getRichStringCellValue().getString().equals("TC3")) {
+	                	TCRowNumbers[2] = row.getRowNum();  
+	                }
+	                else if (cell.getRichStringCellValue().getString().equals("TC4")) {
+	                	TCRowNumbers[3] = row.getRowNum();  
+	                }
+	                else if (cell.getRichStringCellValue().getString().equals("TC5")) {
+	                	TCRowNumbers[4] = row.getRowNum();  
+	                }
+	                else if (cell.getRichStringCellValue().getString().equals("4. Which methods do you need to add to the implementation in order to perform your tests in a convenient manner? Suggest method names!")) {
+	                	TCRowNumbers[5] = row.getRowNum() - 1;  
+	                }
+	            }
+			}
+		}
+		//find the most amount of rows used in a test case
+		int maxTCLength = 0;
+		for(int i = 0; i < TCRowNumbers.length-1; i++) {
+			if(TCRowNumbers[i+1] - TCRowNumbers[i] > maxTCLength) {
+				maxTCLength = TCRowNumbers[i+1] - TCRowNumbers[i];
+			}
+		}
+		
+		//System.out.println(maxTCLength);
+		
+		String [][] TCMap = new String[5][maxTCLength];
+		for(int i = 0; i < 5; i++) {
+			int temp = 0;
+			for(int j = TCRowNumbers[i]; j < TCRowNumbers[i+1]; j++) {
+				TCMap[i][temp] = getCellData(wb, j, 10);
+				temp++;
+			}
+		}
+		
+		for(int i = 0; i < TCMap.length; i++) {
+			for(int j = 0; j < TCMap[0].length; j++) {
+				System.out.println(TCMap[i][j]);
+			}
+		}
+
+	}
+	
     public static void main( String[] args )
     {
     	Workbook wb = null; 
     	try 
     	{
     		//reading data from a file in the form of bytes  
-			FileInputStream fis = new FileInputStream("/Users/Steini/Desktop/HÍ_vor_2021/Prófun hugbúnaðar/assignment09.xlsx");  
+			FileInputStream fis = new FileInputStream("hbv205m_assignment09.xlsx");  
 			//constructs an XSSFWorkbook object, by buffering the whole stream into the memory  
 			wb = new XSSFWorkbook(fis);
 			
     		//Question 3
-    		for(int i = 30; i <= 49; i++) {
-    			System.out.println(getCellData(fis, wb, i, 10));
-    		}
+    		mapTestCases(wb);
     	}
     	
     	catch(FileNotFoundException e)
